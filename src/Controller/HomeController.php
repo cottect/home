a006426504bad0ee8355a999d620c0fcb9a308ae
Bang Dinh <dinhnhatbang@gmail.com>
2019-08-24 13:20:01 +0700

    Add source

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,16 +2,26 @@
 
 namespace App\Controller;
 
+use App\Form\UserRegisterForm;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class HomeController
+class HomeController extends AbstractController
 {
+    /**
+     * @Route("/", name="home_index")
+     *
+     * @param Request $request
+     * @return Response
+     */
     public function index(Request $request)
     {
-        $form = $this->createForm(UserRegisterFrontendForm::class);
+        $form = $this->createForm(UserRegisterForm::class);
 
         return $this->render(
-            'home/index.html.twig',
+            'index.html.twig',
             [
                 'form' => $form->createView(),
             ]
--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -2,7 +2,29 @@
 
 namespace App\Controller;
 
-class RegisterController
-{
+use App\Form\UserRegisterForm;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+class RegisterController extends AbstractController
+{
+    /**
+     * @Route("/register", name="register_index")
+     *
+     * @param Request $request
+     * @return Response
+     */
+    public function index(Request $request)
+    {
+        $form = $this->createForm(UserRegisterForm::class);
+
+        return $this->render(
+            'register.html.twig',
+            [
+                'form' => $form->createView(),
+            ]
+        );
+    }
 }
